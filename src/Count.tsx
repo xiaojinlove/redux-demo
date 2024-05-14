b210eefb16c2fdac79ccd67e6cc603c26e2d8777
gaoyan <magaoyanxj@gmail.com>
2024-05-14 16:01:22 +0800

    使用Redux实现数字加减法-使用useSelector和useDispatch

--- src/Count.tsx
+++ src/Count.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { StateType } from './store';
+import { increase, decrease } from './store/count';
 const Count: FC = () => {
+  const count = useSelector<StateType>(state => state.count)
+  
+  const dispatch = useDispatch()
+  
   return (
     <div>
-      <span>count</span>
-      <button>+</button>
-      <button>-</button>
+      <span><>count: {count}</></span>
+      <button onClick={() => dispatch(increase())}>+</button>
+      <button onClick={() => dispatch(decrease())}>-</button>
     </div>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countReducer from './count'
 
+export type StateType = {
+  count: number
+}
+
 export default configureStore({
   reducer: {
     count: countReducer
