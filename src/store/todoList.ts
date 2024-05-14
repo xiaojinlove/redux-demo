0b79eb37c898937f91d310fd5fab6847b884ddd7
gaoyan <magaoyanxj@gmail.com>
2024-05-14 16:42:54 +0800

    使用Redux实现TodoList-展示复杂数据结构的Reducer写法

---
+++ src/store/todoList.ts
@@ -0,0 +1,43 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { nanoid } from "nanoid"
+
+export type TodoItemType = {
+  id: string
+  title: string
+  completed: boolean
+}
+
+const INIT_STATE: TodoItemType[] = [
+  { id: nanoid(5), title: '吃饭', completed: false},
+  { id: nanoid(5), title: '睡觉', completed: false}
+]
+
+export const todoListSlice = createSlice({
+  name: 'todoList',
+  initialState: INIT_STATE,
+  reducers: {
+    addTodo(state: TodoItemType[], action: PayloadAction<TodoItemType>) {
+      //return state.concat(action.payload) //把newTodo添加到list最后一个
+      return [
+        action.payload, //把newTodo添加到list第一个
+        ...state
+      ]
+    },
+    removeTodo(state: TodoItemType[], action: PayloadAction<{ id: string }>) {
+      const { id: removeId } = action.payload
+      return state.filter(todo => todo.id !== removeId)
+    },
+    toggleCompleted(state: TodoItemType[], action: PayloadAction<{ id: string }>) {
+      const { id: toggleId } = action.payload
+
+      return state.map(todo => {
+        const { id, completed } = todo
+        if(id!== toggleId) return todo
+        return {
+          ...todo,
+          completed: !completed
+        }
+      })
+    }
+  }
+})
